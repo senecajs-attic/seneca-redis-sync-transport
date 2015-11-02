77bbe46ae5a28a19d577b1e71bfd8df77e7225d0
Cristian Kiss <cristian.kiss@nearform.com>
2015-11-02 13:31:37 +0200

    use lab to run tests and lint; removed mocha; formatting

--- redis-sync-transport.js
+++ redis-sync-transport.js
@@ -1,10 +1,8 @@
 /* Copyright (c) 2014-2015 Cristian Ianto, Richard Rodger, MIT License */
-'use strict';
-
+'use strict'
 
 var _ = require('lodash')
 var redis = require('redis')
-
 
 module.exports = function (options) {
   var seneca = this
@@ -12,7 +10,8 @@ module.exports = function (options) {
 
   var so = seneca.options()
 
-  options = seneca.util.deepextend({
+  options = seneca.util.deepextend(
+    {
       'redis-sync': {
         timeout: so.timeout ? so.timeout - 555 : 22222,
         type: 'redis-sync',
@@ -24,15 +23,12 @@ module.exports = function (options) {
     options
   )
 
-
   var tu = seneca.export('transport/utils')
-
 
   seneca.add({role: 'transport', hook: 'listen', type: 'redis-sync'}, hook_listen_redis)
   seneca.add({role: 'transport', hook: 'client', type: 'redis-sync'}, hook_client_redis)
 
-
-  function hook_listen_redis(args, done) {
+  function hook_listen_redis (args, done) {
     var seneca = this
     var type = args.type
     var listen_options = seneca.util.clean(_.extend({}, options[type], args))
@@ -45,7 +41,7 @@ module.exports = function (options) {
     handle_events(redis_out)
     handle_events(redis_sync)
 
-    function sync_handle(msgid, done) {
+    function sync_handle (msgid, done) {
       // using incr to synchronize listeners
       // first incr gets to process the message
       redis_sync.incr('seneca/transport/' + msgid, function (err, result) {
@@ -53,14 +49,14 @@ module.exports = function (options) {
           seneca.log.error('transport', 'redis-sync-key', err)
           return done(err)
         }
-        return done(null, 1 === result)
+        return done(null, result === 1)
       })
     }
 
-    function post_handle(msgid) {
+    function post_handle (msgid) {
       // cleanup
       redis_sync.expire('seneca/transport/' + msgid, 60, function (err, result) {
-        if (err || 1 !== result) {
+        if (err || result !== 1) {
           seneca.log.error('transport', 'redis-sync-key-expire', err || new Error('expected 1 got ' + result))
         }
       })
@@ -72,9 +68,13 @@ module.exports = function (options) {
 
       var msgid = data.origin + '/' + data.id
       sync_handle(msgid, function (err, handle) { // calls back with true if the message should be handled
+        if (err) {
+          seneca.log.error('transport', 'redis-sync-handle-message', err)
+          return done(err)
+        }
         if (handle) {
           tu.handle_request(seneca, data, listen_options, function (out) {
-            if (null == out) return;
+            if (out == null) return
             var outstr = tu.stringifyJSON(seneca, 'listen-' + type, out)
             redis_out.publish(restopic, outstr)
             post_handle(msgid)
@@ -88,7 +88,6 @@ module.exports = function (options) {
       redis_in.subscribe(topic + '_act')
     })
 
-
     seneca.add('role:seneca,cmd:close', function (close_args, done) {
       var closer = this
 
@@ -98,21 +97,19 @@ module.exports = function (options) {
       closer.prior(close_args, done)
     })
 
-
     seneca.log.info('listen', 'open', listen_options, seneca)
 
     done()
   }
 
-
-  function hook_client_redis(args, clientdone) {
+  function hook_client_redis (args, clientdone) {
     var seneca = this
     var type = args.type
     var client_options = seneca.util.clean(_.extend({}, options[type], args))
 
     tu.make_client(make_send, client_options, clientdone)
 
-    function make_send(spec, topic, send_done) {
+    function make_send (spec, topic, send_done) {
       var redis_in = redis.createClient(client_options.port, client_options.host)
       var redis_out = redis.createClient(client_options.port, client_options.host)
 
@@ -141,12 +138,10 @@ module.exports = function (options) {
         redis_out.end()
         closer.prior(close_args, done)
       })
-
     }
   }
 
-
-  function handle_events(redisclient) {
+  function handle_events (redisclient) {
     // Die if you can't connect initially
     redisclient.on('ready', function () {
       redisclient.on('error', function (err) {
--- test/redis-sync-transport.test.js
+++ test/redis-sync-transport.test.js
@@ -1,19 +1,21 @@
 /* Copyright (c) 2014-2015 Richard Rodger */
-'use strict';
 
-// mocha redis-transport.test.js
+'use strict'
+
+var Lab = require('lab')
+var lab = exports.lab = Lab.script()
+var describe = lab.describe
 
 var test = require('seneca-transport-test')
 
+describe('redis-transport', function () {
 
-describe('redis-transport', function() {
-
-  it('happy-any', function( fin ) {
-    test.foo_test( 'redis-sync-transport', require, fin, 'redis-sync', -6379 )
+  lab.test('happy-any', function (fin) {
+    test.foo_test('redis-sync-transport', require, fin, 'redis-sync', -6379)
   })
 
-  it('happy-pin', function( fin ) {
-    test.foo_pintest( 'redis-sync-transport', require, fin, 'redis-sync', -6379 )
+  lab.test('happy-pin', function (fin) {
+    test.foo_pintest('redis-sync-transport', require, fin, 'redis-sync', -6379)
   })
 
 })
